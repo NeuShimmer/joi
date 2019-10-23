73d894829bebf5fa1348c5ec60a9663236357414
纷羽 <gzj0078@icloud.com>
2019-10-23 18:33:36 +0800

    v0.0.4-beta3 debug

--- package/joiType.js
+++ package/joiType.js
@@ -4,8 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var joi_1 = __importDefault(require("@hapi/joi"));
-var __Joi = joi_1["default"];
-exports["default"] = __Joi;
 var Type = /** @class */ (function () {
     function Type() {
         this._A = {};
@@ -18,3 +16,8 @@ function validate(schema, value) {
     return schema.validate(value).value;
 }
 exports.validate = validate;
+joi_1["default"]['combine'] = function (args) {
+    return args;
+};
+var __Joi = joi_1["default"];
+exports["default"] = __Joi;
